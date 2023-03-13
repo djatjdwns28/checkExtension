b194a103782f5d5b66a47e72465925744b72142a
djatjdwns <djatjdwns28@gmail.com>
2023-03-14 00:38:38 +0900

    fix : path 모듈 설정 빠짐

--- api/app.js
+++ api/app.js
@@ -24,12 +24,12 @@ app.set('port', 3000);
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/', indexRouter)
 app.use('/extension', extensionsRouter);
 app.use(history())
-app.use(express.static(path.join(__dirname, 'public')));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
